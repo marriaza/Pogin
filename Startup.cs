32057762d544ffcaad5a03963fd9efb0ea12f2df
Morgan <mo_gies97@hotmail.com>
2018-05-18 14:58:42 -0600

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pogin.Startup))]
+namespace Pogin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
